605b8bc2dfd39cbfbdef272dfca4dd6d16391fe0
Ife <ife.ifeoluwa@gmail.com>
2023-03-24 00:55:23 -0700

    trailing space removed

--- run_monty.c
+++ run_monty.c
@@ -115,7 +115,9 @@ int run_monty(FILE *script_fd)
 	size_t len = 0, exit_status = EXIT_SUCCESS;
 	unsigned int line_number = 0, prev_tok_len = 0;
 	void (*op_func)(stack_t**, unsigned int);
+
 	void getline(&line, &len, script_fd);
+
 
 	if (init_stack(&stack) == EXIT_FAILURE)
 		return (EXIT_FAILURE);

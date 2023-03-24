02aa57373f4374ab7908a7a31b1ee3ce0b53115d
Ife <ife.ifeoluwa@gmail.com>
2023-03-24 00:48:49 -0700

    fixing bugs

--- run_monty.c
+++ run_monty.c
@@ -115,6 +115,7 @@ int run_monty(FILE *script_fd)
 	size_t len = 0, exit_status = EXIT_SUCCESS;
 	unsigned int line_number = 0, prev_tok_len = 0;
 	void (*op_func)(stack_t**, unsigned int);
+	void getline(&line, &len, script_fd);
 
 	if (init_stack(&stack) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
@@ -166,4 +167,4 @@ int run_monty(FILE *script_fd)
 
 	free(line);
 	return (exit_status);
-}	
+}
